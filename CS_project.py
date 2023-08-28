ea4511004f7e60c4240d2814da0b6b8a79fc5024
Aiden Augustine <aidenaugustine777@gmail.com>
2023-08-28 12:52:47 +0530

    mind the pass

--- CS_project.py
+++ CS_project.py
@@ -1,1 +1,8 @@
-print("welcome")
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost",user="root",password="bruh",database="academia")
+
+mycursor = mydb.cursor()
+mycursor.execute("show tables")
+for i in mycursor:
+    print(i)
