c760966bf9892a20edf33fe5aa928f8dc92b51d3
MaxExploit06 <108565261+MaxExploit06@users.noreply.github.com>
2023-08-30 17:20:20 +0530

    Update CS_project.py

--- CS_project.py
+++ CS_project.py
@@ -1,7 +1,48 @@
 import mysql.connector as mc
-mydb = mc.connect(host="localhost",user="root",password="root",database="eSports")
+mydb = mc.connect(host="localhost",user="root",password="root",database="alpha")
 cur = mydb.cursor()
 mydb.autocommit = True
 if mydb.is_connected():
     print('Succesfully connected')
 
+print('''------------------------------
+ESPORTS TEAM MANAGEMENT SYSTEM
+------------------------------
+
+Welcome
+
+1. Display upcoming matches
+2. Display team score
+3. Add a player
+4. Edit a team
+5. Search a player
+6. Update player details
+7. Remove a player
+8. Exit
+
+(To see this guide again, type in 'guide')
+''')
+
+def menu():
+    x=input('Enter your choice:')
+    if x=='1':
+        pass
+    elif x=='2':
+        pass
+    elif x=='3':
+        pass
+    elif x=='4':
+        pass
+    elif x=='5':
+        pass
+    elif x=='6':
+        pass
+    elif x=='7':
+        pass
+    elif x=='8':
+        exit()
+    elif x=='guide':
+        pass
+    else:
+        pass
+    menu()
