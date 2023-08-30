e7a9cae0cc01d89fae4f4b0cce5fe0fa77754177
MaxExploit06 <108565261+MaxExploit06@users.noreply.github.com>
2023-08-30 17:07:05 +0530

    Update CS_project.py

--- CS_project.py
+++ CS_project.py
@@ -1,9 +1,7 @@
 import mysql.connector as mc
-mydb = mc.connect(host="localhost",user="Fowl",password="root",database="eSports")
+mydb = mc.connect(host="localhost",user="root",password="root",database="eSports")
 cur = mydb.cursor()
 mydb.autocommit = True
+if mydb.is_connected():
+    print('Succesfully connected')
 
-cur.execute("desc team1")
-for i in cur:
-    print(i)
-    
