11d49fb931051e33fa330695d7a7d645f2967f65
MaxExploit06 <108565261+MaxExploit06@users.noreply.github.com>
2023-08-30 17:59:40 +0530

    Update CS_project.py

--- CS_project.py
+++ CS_project.py
@@ -11,7 +11,7 @@ def upcoming_matches():
         team='team1'
     elif t==2:
         team='team2'
-    q_1='SELECT * FROM schedule WHERE Team={team}'
+    q_1=f'SELECT * FROM schedule WHERE Team={team}'
     cur.execute(q_1)
     o=cur.fetchall
     for i in o:
@@ -39,7 +39,7 @@ Welcome
 def menu():
     x=input('Enter your choice:')
     if x=='1':
-        upcoming-matches()
+        upcoming_matches()
     elif x=='2':
         pass
     elif x=='3':
